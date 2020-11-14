d69109ebb62ba372927f51cd77e8e4f3b5382956
henval <henri.valencia@jpj.com>
2020-11-14 19:48:39 +0800

    add last exercise file

---
+++ src/main/java/com/gamesys/collection/set/LastExercise.java
@@ -0,0 +1,22 @@
+package com.gamesys.collection.set;
+
+public class LastExercise {
+
+    /**
+     * Using Math.random display 20 numbers
+     * in ascending order
+     * Math.floor(Math.random() * 100)
+     *
+     * Output must be "index : randomNumber"
+     * index must start with number 1
+     * i.e.:
+     *       1 : 2
+     *       2 : 14
+     *       3 : 25
+     *       4 : 28
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
